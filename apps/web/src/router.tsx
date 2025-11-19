908c05dbaa4da214afab08ede49d8bd1e4f9d1ef
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2025-11-18 21:05:53 -0300

    feat [web]: add react-query support

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -1,11 +1,21 @@
+import { QueryClient } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
+import { setupRouterSsrQueryIntegration } from "@tanstack/react-router-ssr-query";
 import { routeTree } from "./routeTree.gen";
 
 export const getRouter = () => {
+	const queryClient = new QueryClient();
+
 	const router = createRouter({
 		routeTree,
 		scrollRestoration: true,
 		defaultPreloadStaleTime: 0,
+		context: { queryClient },
+	});
+
+	setupRouterSsrQueryIntegration({
+		router,
+		queryClient,
 	});
 
 	return router;
--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,8 +1,15 @@
 import { TanStackDevtools } from "@tanstack/react-devtools";
-import { createRootRoute, HeadContent, Scripts } from "@tanstack/react-router";
+import type { QueryClient } from "@tanstack/react-query";
+import {
+	createRootRouteWithContext,
+	HeadContent,
+	Scripts,
+} from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 
-export const Route = createRootRoute({
+export const Route = createRootRouteWithContext<{
+	queryClient: QueryClient;
+}>()({
 	head: () => ({
 		meta: [
 			{
